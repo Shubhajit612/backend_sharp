d28673b2df9e0b011c7e7fae7e3d8afb85c91990
Shubhajit612 <shubhajit612@gmail.com>
2023-08-09 21:56:04 +0530

    Server created..

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("hello World");
+const http = require('http');
+
+// function requestListner(req,res){
+
+// }
+
+
+let server = http.createServer((req,res)=>{
+    console.log("Shubhajit Das");
+});
+
+server.listen(4000);
