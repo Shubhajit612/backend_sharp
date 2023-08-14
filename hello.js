231c1ec2ad7ab31d6d66e129da6cd2145240e6f5
Shubhajit612 <shubhajit612@gmail.com>
2023-08-14 20:47:42 +0530

    Demo project

--- hello.js
+++ hello.js
@@ -7,36 +7,54 @@ let server = http.createServer((req,res)=>{
     const url = req.url;
     const method = req.method;
     if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-        res.write('</html>');
-        return res.end();
+        fs.readFile('message.txt',{encoding:"utf8"},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log('data from file' + data);
+            res.write('<html>');
+            res.write('<head><title>Enter Message</title></head>');
+            res.write(`<body><h1>${data}</h1></body>`);
+            res.write(
+                '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+                );
+
+            res.write('</html>');
+            return res.end();
+          
+        });
+      
     }
     
 
-    if(url === '/message' && method === 'POST'){
+    else if(url === '/message' && method === 'POST'){
         const body = [];
         req.on('data',(chunk)=>{
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end',()=>{
+        return req.on('end',()=>{
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt',message);
+            fs.writeFile('message.txt',message,(err)=>{
+                if(err){
+                    console.log(err);
+                }
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
         });
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
+      
     }
-    res.setHeader('Content-Type','text/html');
+    else {res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>First Page</title></head>');
     res.write('<body><h1>Hello My name is Shubhajit</h1></body>')
     res.write('</html>');
     res.end();
+}
 });
 
 server.listen(4000);
