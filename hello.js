c83a91e98226e3b6dd052b660fb414cfbba3d60e
Shubhajit612 <shubhajit612@gmail.com>
2023-08-24 22:10:28 +0530

    body parsing..

--- hello.js
+++ hello.js
@@ -2,17 +2,24 @@
 
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("Shubhajit ");
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><input type='text' name ='title'><br><input type='number' name='size'><br><button type='submit'>Add Product</button></form>");
 });
 
+app.post('/product',(req,res,next) =>{
+    console.log(req.body);
+    res.redirect('/');
+})
 
-app.use((req,res,next)=>{
-    console.log("Sejuti ");
-    res.send({key1:"value"});
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Add-Product..!!</h1>");
 });
 
 
